a1ff36144c5b3bc6b514220de5eab8096d8d5af5
allijasuba <allijasuba@gmail.com>
2022-08-08 16:56:58 +0800

    my project

---
+++ 1.R
@@ -0,0 +1,5 @@
+addition <- function(a,b){a+b}
+subtraction <- function(a,b){a-b}
+multiplication <- function(a,b){a*b}
+division <- function(a,b){a/b}
+
---
+++ 2.R
@@ -0,0 +1,11 @@
+#Defining a function
+addition <- function(a,b){a**2+b**2}
+subtraction <- function(a,b){a**2-b**2}
+multiplication <- function(a,b){a**2*b**2}
+division <- function(a,b){a**2/b**2}
+
+#Invoking a function
+addition(5,7)
+subtraction(4,6)
+multiplication(3,2)
+division(12,6)
